c9502bbe299e8a86ad9897bc9110a8396b2bb73b
RogerCastillo877 <roger.castillo877@gmail.com>
2022-11-09 13:52:35 -0500

    test state and actions of authSlice

---
+++ tests/fixtures/testUser.js
@@ -0,0 +1,6 @@
+export const testUserCredentials = {
+  email: 'test@gmail.com',
+  password: '1234',
+  uid: '636bd299fef6c8819677274d',
+  name: 'Test User'
+}
--- tests/store/auth/authSlice.test.js
+++ tests/store/auth/authSlice.test.js
@@ -1,10 +1,52 @@
-import { authSlice } from "../../../src/store/auth/authSlice";
-import { initialState } from "../../fixtures/authStates";
+import { authSlice, clearErrorMessage, onLogin, onLogout } from "../../../src/store/auth/authSlice";
+import { authenticatedState, initialState } from "../../fixtures/authStates";
+import { testUserCredentials } from "../../fixtures/testUser";
 
 describe('should test authSlice', () => {
 
   test('should return initial state', () => {
 
     expect( authSlice.getInitialState() ).toEqual( initialState );
+  });
+
+  test('should do login', () => {
+
+    const state = authSlice.reducer( initialState, onLogin( testUserCredentials ) );
+    expect( state ).toEqual({
+      status: 'authenticated',
+      user: testUserCredentials,
+      errorMessage: undefined
+    })
+  });
+
+  test('should do logout', () => {
+    const state = authSlice.reducer( authenticatedState, onLogout() );
+
+    expect( state ).toEqual({
+      status:'not-authenticated',
+      user: {},
+      errorMessage: undefined
+    })
+  });
+  
+  test('should do logout with message', () => {
+    
+    const errorMessage = 'Credenciales no válidas'
+    const state = authSlice.reducer( authenticatedState, onLogout( errorMessage ) );
+
+    expect( state ).toEqual({
+      status:'not-authenticated',
+      user: {},
+      errorMessage: errorMessage
+    })
+  });
+
+  test('should clean error message', () => {
+
+    const errorMessage = 'Credenciales no válidas'
+    const state = authSlice.reducer( authenticatedState, onLogout( errorMessage ) );
+    const newState = authSlice.reducer( state, clearErrorMessage() );
+
+    expect( newState.errorMessage ).toBe( undefined );
   });
 });
