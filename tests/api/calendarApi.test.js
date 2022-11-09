44b3212ba7b1ade9ad22c445fa8cc8d4c89b002e
RogerCastillo877 <roger.castillo877@gmail.com>
2022-11-09 13:09:38 -0500

    AXIOS config

---
+++ tests/api/calendarApi.test.js
@@ -0,0 +1,17 @@
+import { calendarAPi } from "../../src/api";
+
+describe('should test calendarApi', () => {
+  
+  test('should have config for defect', () => {
+    expect( calendarAPi.defaults.baseURL ).toBe( process.env.VITE_API_URL );
+  });
+
+  test('should have x-token in header of request', async() => {
+    
+    const token = 'ABC-123_XYZ';
+    localStorage.setItem( 'token', token );
+    const res = await calendarAPi.get( '/auth ' );
+
+    expect( res.config.headers['x-token'] ).toBe( token );
+  });
+});
